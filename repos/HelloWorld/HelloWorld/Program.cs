8c5dfebad833df4f9c790ddeb5fac3c0ba623e20
krishnamohan1 <krishna.g.mohan@accenture.com>
2017-09-12 15:22:22 +0530

    pushed mscode

---
+++ repos/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("\nWhat is your name?");
+			var name = Console.ReadLine();
+			var date = DateTime.Now;
+			Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+			Console.WriteLine("\n press key to exist");
+		    Console.ReadKey(true);
+		}
+    }
+}
